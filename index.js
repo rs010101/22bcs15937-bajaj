cda5c701f059ae4218c8248e35fd7c6c91b86136
Radhika Singla <radhika01singhal04@gmail.com>
2025-02-21 20:27:09 +0530

    backend

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const USER_ID = "radhika_s01";
 const EMAIL = "radhika01singhal04@gmail.com";
